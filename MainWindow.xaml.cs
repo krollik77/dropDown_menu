47f9c24f53ba032e70b81303b410192de8ca1ebe
krollik77 <krollik77@ya.ru>
2021-09-29 15:28:15 +0300

    версия 1

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BeautySolutions.View.ViewModel;
+using MaterialDesignThemes.Wpf;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +25,51 @@ namespace dropDown_menu
         public MainWindow()
         {
             InitializeComponent();
+
+            var item0 = new ItemMenu("Dashboard", new UserControl(), PackIconKind.ViewDashboard);
+
+            var menuRegister = new List<SubItem>();
+            menuRegister.Add(new SubItem("Мой профиль"));
+            menuRegister.Add(new SubItem("Настройка"));
+            var item1 = new ItemMenu("Регистрация", menuRegister, PackIconKind.Register);
+
+            var menuSchedule = new List<SubItem>();
+            menuSchedule.Add(new SubItem("Питта"));
+            menuSchedule.Add(new SubItem("Вата"));
+            menuSchedule.Add(new SubItem("Капха"));
+            var item2 = new ItemMenu("Твоя Доша", menuSchedule, PackIconKind.Schedule);
+
+            var menuReports = new List<SubItem>();
+            menuReports.Add(new SubItem("Муладхара"));
+            menuReports.Add(new SubItem("Свадхистана"));
+            menuReports.Add(new SubItem("Манипура"));
+            menuReports.Add(new SubItem("Анахата"));
+            menuReports.Add(new SubItem("Вишудха"));
+            menuReports.Add(new SubItem("Аджна"));
+            menuReports.Add(new SubItem("Сахасрара"));
+            var item3 = new ItemMenu("Чакры", menuReports, PackIconKind.FileReport);
+
+            var menuExpenses = new List<SubItem>();
+            menuExpenses.Add(new SubItem("Избранное"));
+            menuExpenses.Add(new SubItem("Мои покупки"));
+            var item4 = new ItemMenu("Корзина", menuExpenses, PackIconKind.ShoppingBasket);
+
+            var menuFinancial = new List<SubItem>();
+            menuFinancial.Add(new SubItem("Вконтакте"));
+            menuFinancial.Add(new SubItem("Инстаграм"));
+            menuFinancial.Add(new SubItem("FaceBoock"));
+            var item5 = new ItemMenu("Соц.Сети", menuFinancial, PackIconKind.ScaleBalance);
+
+            
+
+            Menu.Children.Add(new UserControl_menu(item0));
+            Menu.Children.Add(new UserControl_menu(item1));
+            Menu.Children.Add(new UserControl_menu(item2));
+            Menu.Children.Add(new UserControl_menu(item3));
+            Menu.Children.Add(new UserControl_menu(item4));
+            Menu.Children.Add(new UserControl_menu(item5));
+           
         }
     }
 }
+
---
+++ UserControl_menu.xaml.cs
@@ -0,0 +1,33 @@
+﻿using BeautySolutions.View.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace dropDown_menu
+{
+    /// <summary>
+    /// Логика взаимодействия для UserControl_menu.xaml
+    /// </summary>
+    public partial class UserControl_menu : UserControl
+    {
+        public UserControl_menu(ItemMenu itemMenu)
+        {
+            InitializeComponent();
+            ExpanderMenu.Visibility = itemMenu.SubItems == null ? Visibility.Collapsed : Visibility.Visible;
+            ListViewItemMenu.Visibility = itemMenu.SubItems == null ? Visibility.Visible : Visibility.Collapsed;
+
+            this.DataContext = itemMenu;
+        }
+    }
+}
